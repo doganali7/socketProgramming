f22339ec8dc4f21a2ec337e21c9adbd975289f2d
Ali Dogan <alidogn98@gmail.com>
2022-06-01 02:44:52 +0300

    Ftp server can now send back whatever it receives and listens on the port

--- ftpserver.c
+++ ftpserver.c
@@ -76,16 +76,55 @@ int main(int argc, char *argv[])
             return (1);
         }
 
-        memset( &local_addr, 0, sizeof(local_addr) );
-        memset( &remote_addr, 0, sizeof(remote_addr) );
+        memset(&local_addr, 0, sizeof(local_addr));
+        memset(&remote_addr, 0, sizeof(remote_addr));
 
         /* Set values to local_addr structure */
         local_addr.sin_family = AF_INET;
-        local_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+        local_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
         local_addr.sin_port = htons(port);
 
         // set SO_REUSEADDR on a socket to true (1):
         optval = 1;
+        // printf("%s\n",local_addr.sin_addr.s_addr);
+
+        setsockopt(server, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof optval);
+
+        if (bind(server, (struct sockaddr *)&local_addr, sizeof(local_addr)) < 0)
+        {
+            /* could not start server */
+            perror("Bind error");
+            return (1);
+        }
+
+        if (listen(server, SOMAXCONN) < 0)
+        {
+            perror("listen");
+            exit(1);
+        }
+
+        printf("Concurrent  socket server now starting on port %d\n", port);
+        printf("Wait for connection\n");
+        while (1)
+        {
+            length = sizeof remote_addr;
+            if ((fd = accept(server, (struct sockaddr *)&remote_addr, &length)) == -1)
+            {
+                perror("Accept Problem!");
+                continue;
+            }
+            printf("Server: got connection from %s\n",
+                   inet_ntoa(remote_addr.sin_addr));
+            if ((pid = fork()) == 0)
+            {
+                close(server);
+                do_job(fd);
+                printf("Child finished their job!\n");
+                close(fd);
+                exit(0);
+            }
+        }
+
+        close(server);
     }
 }
-
